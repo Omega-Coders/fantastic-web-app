e71a34e1a8e9bfb936d5da815df67cdb24797b7a
Mahesh-Andey <97396160+Mahesh-Andey@users.noreply.github.com>
2022-05-23 11:01:03 +0530

    login ui

--- fantastic/src/App.js
+++ fantastic/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from './Screens/login.js';
 import Intro from './Screens/Intro';
 import Form from './Screens/form';
-
+import FileUploadPage from './Screens/cropper';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
              <Route path="/" element={<Intro />} />
              <Route path="/login" element={<Login />} />
              <Route path="/form" element={<Form />} />
+             <Route path="/cropper" element={<FileUploadPage />} />
            </Routes>
          </Router>
        </div>
---
+++ fantastic/src/Screens/cropper.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function FileUploadPage(){
+	return(
+   <div>
+       <div class="form-group">
+           <input type="file" name="file" className="file-input" />
+       </div>
+			
+			
+		</div>
+	)
+}
+
+
+export default FileUploadPage;
